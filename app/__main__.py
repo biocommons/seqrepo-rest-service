2eed6437ece4bb1386e0a04bb7936419366aebd2
Reece Hart <reecehart@gmail.com>
2019-05-19 17:53:51 -0700

    added `seqrepo-rest-service` console script to start the rest service

---
+++ app/__main__.py
@@ -0,0 +1,32 @@
+"""start refget webservice
+
+"""
+
+import logging
+
+from pkg_resources import resource_filename
+
+import connexion
+from flask import Flask, redirect
+
+
+def main():
+    cxapp = connexion.App(__name__, debug=True)
+
+    spec_fn = "refget/refget-openapi.yaml"
+    cxapp.add_api(spec_fn,
+                  validate_responses=True,
+                  strict_validation=True)
+
+    @cxapp.route('/')
+    def index():
+        return redirect("/refget/1/ui")
+
+    cxapp.run(host="0.0.0.0",
+              extra_files=[spec_fn],
+              processes=1)
+
+
+
+if __name__ == "__main__":
+    main()
