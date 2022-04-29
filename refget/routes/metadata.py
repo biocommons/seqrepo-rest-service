87640eddf7c6f27cf585c5bd6bf10adc3c9261b7
Katie Stahl <kathryn.stahl@nationwidechildrens.org>
2022-04-29 15:32:08 -0400

    fix

---
+++ refget/routes/metadata.py
@@ -0,0 +1,39 @@
+import logging
+
+from connexion import NoContent, request
+
+from ...threadglobals import get_seqrepo
+from ...utils import get_sequence_id, base64url_to_hex, problem, valid_content_types
+
+
+_logger = logging.getLogger(__name__)
+
+
+def get(id):
+    accept_header = request.headers.get("Accept", None)
+    if accept_header and accept_header not in valid_content_types:
+        _logger.warn(f"{accept_header} not valid")
+        return problem(406, "Invalid Accept header")
+    
+    sr = get_seqrepo()
+    seq_id = get_sequence_id(sr, id)
+    if not seq_id:
+        return NoContent, 404
+    seqinfo = sr.sequences.fetch_seqinfo(seq_id)
+    aliases = sr.aliases.fetch_aliases(seq_id)
+
+    md5_rec = [a for a in aliases if a["namespace"] == "MD5"]
+    md5_id = md5_rec[0]["alias"] if md5_rec else None
+
+    md = {
+        "id": seq_id,
+        "md5": md5_id,
+        "trunc512": base64url_to_hex(seq_id),
+        "length": seqinfo["len"],
+        "aliases": [
+            {"naming_authority": a["namespace"], "alias": a["alias"]}
+            for a in aliases
+            ]
+        }
+
+    return {"metadata": md}, 200
---
+++ seqrepo/routes/ping.py
@@ -0,0 +1,26 @@
+from ...threadglobals import get_seqrepo
+
+import biocommons.seqrepo 
+import bioutils
+
+from pkg_resources import get_distribution
+
+
+def get():
+    sr = get_seqrepo()
+    
+    return {
+        "version": get_distribution("seqrepo-rest-service").version,
+        "url": "https://github.com/biocommons/seqrepo-rest-service/",
+        "dependencies": {
+            "seqrepo": {
+                "version": biocommons.seqrepo.__version__,
+                "root": sr._root_dir,
+                "url": "https://github.com/biocommons/biocommons.seqrepo/",
+            },
+            "bioutils": {
+                "version": bioutils.__version__,
+                "url": "https://github.com/biocommons/bioutils/",
+            },
+        }
+    }
