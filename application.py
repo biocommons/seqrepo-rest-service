4a1157b5b42ae190c26f010850a9fc7adec620e3
korikuzma <korikuzma@gmail.com>
2022-05-01 16:41:26 -0400

    wip: revert eb configs, move and rename __main__ to application in root dir

--- application.py
+++ application.py
@@ -17,6 +17,9 @@ _logger = logging.getLogger(__name__)
 # __version__ = get_distribution("seqrepo-rest-service").version
 __version__ = "1.0.0"
 APP_ROOT = Path(__file__).resolve().parents[0]
+print()
+print(APP_ROOT)
+print()
 
 
 def main():
@@ -25,38 +28,39 @@ def main():
     if "SEQREPO_DIR" not in os.environ:
         _logger.warn("SEQREPO_DIR is undefined; rest service will use `latest`")
 
-    cxapp = connexion.App(__name__, debug=True)
-    cxapp.app.url_map.strict_slashes = False
+    application = connexion.App(__name__, debug=True)
+    application.app.url_map.strict_slashes = False
 
     spec_files = []
 
     # seqrepo interface
-    spec_fn = f"{APP_ROOT}/seqrepo/openapi.yaml"
-    cxapp.add_api(spec_fn,
+    spec_fn = f"{APP_ROOT}/seqrepo_rest_service/seqrepo/openapi.yaml"
+    print(spec_fn)
+    application.add_api(spec_fn,
                   validate_responses=True,
                   strict_validation=True)
     spec_files += [spec_fn]
 
-    @cxapp.route('/')
-    @cxapp.route('/seqrepo')
+    @application.route('/')
+    @application.route('/seqrepo')
     def seqrepo_ui():
         return redirect("/seqrepo/1/ui/")
 
 
     # refget interface
-    spec_fn = f"{APP_ROOT}/refget/refget-openapi.yaml"
-    cxapp.add_api(spec_fn,
+    spec_fn = f"{APP_ROOT}/seqrepo_rest_service/refget/refget-openapi.yaml"
+    application.add_api(spec_fn,
                   validate_responses=True,
                   strict_validation=True)
     spec_files += [spec_fn]
 
-    @cxapp.route('/refget')
+    @application.route('/refget')
     def refget_ui():
         return redirect("/refget/1/ui/")
 
 
     _logger.info("Also watching " + str(spec_files))
-    cxapp.run(host="0.0.0.0",
+    application.run(host="0.0.0.0",
               extra_files=spec_files)
 
 if __name__ == "__main__":
