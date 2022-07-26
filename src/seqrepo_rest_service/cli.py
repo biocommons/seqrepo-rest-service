b91ee8284932820977d68d486d48ed54e877ae18
Reece Hart <reecehart@gmail.com>
2022-07-26 14:29:22 -0400

    add --wait-for-path option to facilitate docker-compose startup

--- src/seqrepo_rest_service/cli.py
+++ src/seqrepo_rest_service/cli.py
@@ -1,11 +1,12 @@
-"""start refget webservice
+"""start seqrepo rest service
 
 """
 
-from genericpath import isdir
+import argparse
 import logging
 import os
-import sys
+import pathlib
+import time
 
 from pkg_resources import get_distribution, resource_filename
 
@@ -15,8 +16,29 @@ import connexion
 from flask import Flask, redirect
 
 
+WAIT_POLL_PERIOD = 15  # seconds between polling for SEQREPO PATH
+
+
 _logger = logging.getLogger(__name__)
 __version__ = get_distribution("seqrepo-rest-service").version
+
+
+def _parse_opts():
+    ap = argparse.ArgumentParser(description=__doc__.split()[0])
+    ap.add_argument(
+        "SEQREPO_INSTANCE_DIR",
+        type=pathlib.Path,
+        help="SeqRepo instance directory (e.g., /usr/local/share/seqrepo/2021-01-29)",
+    )
+    ap.add_argument(
+        "--wait-for-path",
+        "-w",
+        action="store_true",
+        default=False,
+        help="Wait for path to exist before starting (useful for docker-compose)",
+    )
+    opts = ap.parse_args()
+    return opts
 
 
 def main():
@@ -24,14 +46,16 @@ def main():
 
     if "SEQREPO_DIR" in os.environ:
         _logger.warn("SEQREPO_DIR environment variable is now ignored")
-        
-    # TODO: use argparse
-    if len(sys.argv) != 2:
-        raise RuntimeError("Usage: seqrepo-rest-service <dir>")
 
-    seqrepo_dir = sys.argv[1]
-    _logger.info(f"Using {seqrepo_dir=} from command line")
-    _ = SeqRepo(seqrepo_dir)   # test opening
+    opts = _parse_opts()
+
+    seqrepo_dir = opts.SEQREPO_INSTANCE_DIR
+    if opts.wait_for_path:
+        while not seqrepo_dir.exists():
+            _logger.info(f"{seqrepo_dir}: waiting for existence")
+            time.sleep(WAIT_POLL_PERIOD)
+        _logger.info(f"{seqrepo_dir}: path found")
+    _ = SeqRepo(seqrepo_dir.as_posix())  # test opening
 
     cxapp = connexion.App(__name__, debug=True)
     cxapp.app.url_map.strict_slashes = False
@@ -41,32 +65,26 @@ def main():
 
     # seqrepo interface
     spec_fn = resource_filename(__name__, "seqrepo/openapi.yaml")
-    cxapp.add_api(spec_fn,
-                  validate_responses=True,
-                  strict_validation=True)
+    cxapp.add_api(spec_fn, validate_responses=True, strict_validation=True)
     spec_files += [spec_fn]
 
-    @cxapp.route('/')
-    @cxapp.route('/seqrepo')
+    @cxapp.route("/")
+    @cxapp.route("/seqrepo")
     def seqrepo_ui():
         return redirect("/seqrepo/1/ui/")
 
-
     # refget interface
     spec_fn = resource_filename(__name__, "refget/refget-openapi.yaml")
-    cxapp.add_api(spec_fn,
-                  validate_responses=True,
-                  strict_validation=True)
+    cxapp.add_api(spec_fn, validate_responses=True, strict_validation=True)
     spec_files += [spec_fn]
-     
-    @cxapp.route('/refget')
+
+    @cxapp.route("/refget")
     def refget_ui():
         return redirect("/refget/1/ui/")
 
-    
     _logger.info("Also watching " + str(spec_files))
-    cxapp.run(host="0.0.0.0",
-              extra_files=spec_files)
+    cxapp.run(host="0.0.0.0", extra_files=spec_files)
+
 
 if __name__ == "__main__":
     main()
