20e73c324f0bfabfa717ec630562dd6c9e5a588f
Kyle Ferriter <kferrite@broadinstitute.org>
2023-09-05 12:09:46 -0400

    Use a process-global object for _get_or_create instead of request-scoped flask.g

--- src/seqrepo_rest_service/threadglobals.py
+++ src/seqrepo_rest_service/threadglobals.py
@@ -3,24 +3,22 @@
 """
 
 import logging
-import os
 
 from biocommons.seqrepo import SeqRepo
-from flask import current_app, g
+from flask import current_app
 
 _logger = logging.getLogger(__name__)
 
 
 def get_seqrepo():
     seqrepo_dir = current_app.config["seqrepo_dir"]
-    _logger.info(f"Opening {seqrepo_dir=}")
     return _get_or_create("seqrepo", lambda: SeqRepo(root_dir=seqrepo_dir))
 
 
 def _get_or_create(k, f):
-    k = "_" + k
-    o = getattr(g, k, None)
+    k = '_' + k
+    o = getattr(_get_or_create, k, None)
     if o is None:
         o = f()
-        setattr(g, k, o)
+        setattr(_get_or_create, k, o)
     return o
