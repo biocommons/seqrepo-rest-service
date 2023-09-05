41b6e162e8d83ada938e35aee32d87b3e2ce2fbc
Kyle Ferriter <kferrite@broadinstitute.org>
2023-09-05 12:32:20 -0400

    Add back a log when SeqRepo obj is created

--- src/seqrepo_rest_service/threadglobals.py
+++ src/seqrepo_rest_service/threadglobals.py
@@ -12,13 +12,15 @@ _logger = logging.getLogger(__name__)
 
 def get_seqrepo():
     seqrepo_dir = current_app.config["seqrepo_dir"]
+    if _get_or_create("seqrepo", None, False) is None:
+        _logger.info("Opening seqrepo_dir=%s", seqrepo_dir)
     return _get_or_create("seqrepo", lambda: SeqRepo(root_dir=seqrepo_dir))
 
 
-def _get_or_create(k, f):
-    k = '_' + k
+def _get_or_create(k, f, create=True):
+    k = "_" + k
     o = getattr(_get_or_create, k, None)
-    if o is None:
+    if o is None and create:
         o = f()
         setattr(_get_or_create, k, o)
     return o
